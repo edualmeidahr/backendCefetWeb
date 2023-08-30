3ff07dc6ac3797527c5be998e8ae93cb5a9539b1
edualmeidahr <eduardohenriquecruzeiro123@gmail.com>
2023-08-30 11:26:34 -0300

    Updates

--- app.js
+++ app.js
@@ -2,22 +2,23 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql2');
 const port = 3002
-const bodyParser = require("body-parser")
-const moment = require("moment")
+const moment = require('moment');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
-const connection = mysql.createConnection({
-  host: 'aulascefet.c8tuthxylqic.sa-east-1.rds.amazonaws.com',
-  user: 'aluno',
-  database: 'aulas_web',
+var connection = mysql.createConnection({
+  host     : 'aulascefet.c8tuthxylqic.sa-east-1.rds.amazonaws.com',
+  user     : 'aluno',
   password : 'alunoc3f3t',
+  database : 'aulas_web'
 });
 
-app.get('/', (req, res) => {
-  res.send(`Backend Eduardo Rodando...`);
-})
-app.get('/cliente', function(req, res, next) {
+app.get('/', function(req, res, next) {
+  res.send("Backend Eduardo Almeida...");
+});
+
+app.get('/clientes', function(req, res, next) {
   connection.query(
     'select * from cliente',
     (err, results, fields) => {
@@ -28,18 +29,19 @@ app.get('/cliente', function(req, res, next) {
   );
 });
 
-app.get('/cliente/:id_cliente', (req, res) => {
-  var id_cliente = req.params("id_cliente")
-  connection.query (
-    'select * from cliente where id_cliente = "$(id_cliente)"',
+app.get('/clientes/:id_cliente', function(req, res, next) {
+  var idCliente = req.params['id_cliente'];
+  connection.query(
+    `select * from cliente where id_cliente = ${+idCliente}`,
     (err, results, fields) => {
-      if(err) console.log(err)
+      if(err) 
+        console.log(err)
       res.send(results)
     }
   );
-})
+});
 
-app.get('/cliente/:email', function(req, res, next) {
+app.get('/clientes_email/:email', function(req, res, next) {
   var email = req.params['email'];
   var sql =  `select * from cliente where email = "${email}"`;
   console.log(sql)
@@ -57,43 +59,15 @@ app.get('/cliente/:email', function(req, res, next) {
   );
 });
 
-
-
-app.post('/cliente', (req, res) => {
-  var nome = req.body.nome
-  var sobrenome = req.body.sobrenome
-  var email = req.body.email
-  var data_cadastro  = moment().format("yyyy-mm-dd")
-  var salario = req.body.salario
-  var sql = 'insert into cliente(nome, sobrenome, email, data_cadastro, salario) '+
-  'values("$(nome)", "$(sobrenome)", "$(email)", "$(data_cadastro"), $(salario))'
-    connection.query(sql, (erro, resultado) => {
-      if(erro)  res.send(erro)
-      res.send(resultado)
-    })
-  })
-
-  app.post('/cliente_del/:id_cliente', function(req, res, next) {
-    var idCliente = req.params['id_cliente'];
-    connection.query(
-      `delete from cliente where id_cliente = ${+idCliente}`,
-      (err, results, fields) => {
-        if(err) 
-          console.log(err)
-        res.send(results)
-      }
-    );
-  });
-
-  app.patch('/cliente/:id_cliente', function(req, res, next) {
-    var nome = req.body.nome;
-    var sobrenome = req.body.sobrenome;
-    var email = req.body.email;
-    var salario = +req.body.salario;
-    var idCliente = +req.params.id_cliente;
-    var sql = `
-    update cliente set nome = "${nome}", sobrenome = "${sobrenome}", ` +
-    `email = "${email}", salario = ${salario} where id_cliente = ${idCliente}`
+app.post('/clientes', function(req, res, next) {
+  var nome = req.body.nome;
+  var sobrenome = req.body.sobrenome;
+  var email = req.body.email;
+  var salario = +req.body.salario;
+  var sql = `insert into cliente(nome, sobrenome,` +
+  `email, data_cadastro, salario) values ("${nome}", "${sobrenome}", ` +
+  `"${email}", "${moment().format("YYYY-MM-DD")}", ${salario})`
+  // if(nome != undefined && sobrenome != undefined && email != undefined && salario != undefined){
     connection.query(
       sql, (erro, resultados, fields) => {
         if(erro)
@@ -101,9 +75,38 @@ app.post('/cliente', (req, res) => {
         res.send(resultados)
       }
     )
-  });
+  // }
+});
 
+app.post('/clientes_del/:id_cliente', function(req, res, next) {
+  var idCliente = req.params['id_cliente'];
+  connection.query(
+    `delete from cliente where id_cliente = ${+idCliente}`,
+    (err, results, fields) => {
+      if(err) 
+        console.log(err)
+      res.send(results)
+    }
+  );
+});
 
+app.patch('/clientes/:id_cliente', function(req, res, next) {
+  var nome = req.body.nome;
+  var sobrenome = req.body.sobrenome;
+  var email = req.body.email;
+  var salario = +req.body.salario;
+  var idCliente = +req.params.id_cliente;
+  var sql = `
+  update cliente set nome = "${nome}", sobrenome = "${sobrenome}", ` +
+  `email = "${email}", salario = ${salario} where id_cliente = ${idCliente}`
+  connection.query(
+    sql, (erro, resultados, fields) => {
+      if(erro)
+        res.send(erro)
+      res.send(resultados)
+    }
+  )
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
