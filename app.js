c8d52fe47e8d0cb9ac8d14eca028314556f84323
Eduardo Almeida <eduardohenriquecruzeiro123@gmail.com>
2023-09-01 12:08:41 -0300

    Desenvolvimento de CRUD

--- app.js
+++ app.js
@@ -61,27 +61,35 @@ app.get('/clientes_email/:email', function(req, res, next) {
   );
 });
 
-app.post('/clientes',  (req, res) => {
-  var nome = req.body.nome
-  var sobrenome = req.body.sobrenome
-  var email = req.body.email
-  var data_cadastro = moment().format("YYYY-MM-DD")
-  var salario = req.body.salario
+app.post('/clientes', function(req, res, next) {
+  var nome = req.body.nome;
+  var sobrenome = req.body.sobrenome;
+  var email = req.body.email;
+  var salario = +req.body.salario;
+
   console.log(req.files)
-  var sql = `insert into cliente(nome, sobrenome, email, `+
-        `data_cadastro,salario) values("${nome}", "${sobrenome}", `+
-        `"${email}", "${data_cadastro}", ${salario})`
-   connection.query(sql, (erro, resultado) =>{
-      if(erro) res.send(erro)
-      var caminhoTemp = req.files.avatar.path
-      var caminhoNovo = `./uploads/clientes/${resultado.insertId}.png`
-      fs.copyFile(caminhoTemp, caminhoNovo, (err) => {
-        console.log(err)
-        res.send(resultado)
-      })
-      
-   })
-})
+
+  var sql = `insert into cliente(nome, sobrenome,` +
+  `email, data_cadastro, salario) values ("${nome}", "${sobrenome}", ` +
+  `"${email}", "${moment().format("YYYY-MM-DD")}", ${salario})`
+  // if(nome != undefined && sobrenome != undefined && email != undefined && salario != undefined){
+    connection.query(
+      sql, (erro, resultados, fields) => {
+        if(erro)
+          res.send(erro)
+
+        var caminhoTemp = req.files.avatar.path;
+        var type = req.files.avatar.type.split('/');
+        var caminhoNovo = `./uploads/clientes/cliente${resultados.insertId}.${type[type.length - 1]}`;
+
+        fs.copyFile(caminhoTemp, caminhoNovo, (err) => {
+          console.log(err)
+          res.send(resultados)
+        });
+      }
+    )
+  // }
+});
 
 app.post('/clientes_del/:id_cliente', function(req, res, next) {
   var idCliente = req.params['id_cliente'];
@@ -112,6 +120,86 @@ app.patch('/clientes/:id_cliente', function(req, res, next) {
     }
   )
 });
+
+//fornecedores
+app.get('/fornecedores', (req, res) => {
+  connection.query(
+    'select * from cliente',
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
+
+app.get('/fornecedores/:id_cliente', (req, res) => {
+  var id_fornecedor  = req.params.id_fornecedor  
+  connection.query(
+    `select * from fornecedor where id_fornecedor = ${id_fornecedor }`,
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
+
+app.post('/fornecedores',  (req, res) => {
+  var razao = req.body.razao;
+  var cpf_cnpj = req.body.cpf_cnpj;
+  var contato = req.body.contato;
+  var logradouro = req.body.logradouro || null;
+  var cidade = req.body.cidade || null;
+  var uf = req.body.uf || null;
+  console.log(req.files)
+  var sql = `INSERT INTO fornecedor(razao, cpf_cnpj, contato, logradouro, cidade, uf) 
+    VALUES(
+      "${razao}", 
+      "${cpf_cnpj}", 
+      "${contato}", 
+      "${logradouro}", 
+      "${cidade}", 
+      "${uf}")`;
+
+   connection.query(sql, (erro, resultado) =>{
+      if(erro) res.send(erro)
+        res.send(resultado)
+    })
+  })
+
+app.patch('/fornecedores/:id_fornecedor', (req, res) => {
+  var id_fornecedor = req.params.id_fornecedor;
+  var razao = req.body.razao;
+  var cpf_cnpj = req.body.cpf_cnpj;
+  var contato = req.body.contato;
+  var logradouro = req.body.logradouro || null;
+  var cidade = req.body.cidade || null;
+  var uf = req.body.uf || null;
+  var sql = `
+    UPDATE fornecedor SET 
+      razao="${razao}", 
+      cpf_cnpj="${cpf_cnpj}", 
+      contato="${contato}", 
+      logradouro="${logradouro}", 
+      cidade="${cidade}", 
+      uf="${uf}" 
+    WHERE id_fornecedor=${id_fornecedor}`;
+
+   connection.query(sql, (erro, resultado) =>{
+      if(erro) res.send(erro)
+      res.send(resultado)
+   })
+})
+
+app.post('/fornecedores_delete/:id_fornecedor', (req, res) => {
+  var id_fornecedor  = req.params.id_fornecedor  
+  connection.query(
+    `DELETE FROM fornecedor WHERE id_fornecedor = ${id_fornecedor}`,
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
