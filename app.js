3e12b135c9e399cfd822b902845c2df34679bb42
edualmeidahr <eduardohenriquecruzeiro123@gmail.com>
2023-10-20 11:08:21 -0400

    Atualizando codigo

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const app = express()
 
 const port = 3002
 const moment = require('moment');
+const cors = require("cors")
 const bodyParser = require('body-parser');
 const formData = require("express-form-data")
 const fs = require("fs")
 app.use(formData.parse())
 app.use(bodyParser.urlencoded({extended: true}));
+
+var corsOptions = {
+  origin: 'http://127.0.0.1:5501',
+  optionsSuccessStatus: 200 
+}
+
+app.use(express.static('uploads'))
+app.use(cors())
+
 
 require("./rotas/clientes")(app)
 require("./rotas/fornecedores")(app)
